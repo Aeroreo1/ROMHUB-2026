c144b9eca9fd9061333ca39915cd898248b6d7bd
luc <luc@DESKTOP-SEH07PG>
2026-09-06 17:15:52 +0100

    Created ROM Model

---
+++ ROMHUB 2026/Models/Rom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ROMHUB_2026.Models
+{
+    public class Rom
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Platform { get; set; }
+
+        public string FilePath { get; set; }
+
+        public string CoverImagePath { get; set; }
+
+        public bool IsFavourite { get; set; }
+    }
+}
